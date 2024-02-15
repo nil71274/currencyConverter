1901a00e13329cbe86f7c277aa05dc6124894ec6
Nilesh Pawar <nileshpawar290402@gmail.com>
2024-02-15 19:00:59 +0530

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+import InputBox from './components/InputBox'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const currencyOptions = Object.keys(currencyInfo);
+
+  const swap = ()=> {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  }
+  return (
+    <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+            backgroundImage: `url("https://img.freepik.com/free-photo/cardano-blockchain-platform-concept_23-2150278277.jpg?w=826&t=st=1708002790~exp=1708003390~hmac=bbb3a2a1c46d6e11cb3914f7bc85a0223a4760a68246d9bc0c653908289faced")`,
+        }}
+    >
+        <div className="w-full">
+            <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                       convert()
+                    }}
+                >
+                    <div className="w-full mb-1">
+                        <InputBox
+                            label="From"
+                            amount={amount}
+                            currencyOptions={currencyOptions}
+                            onCurrencyChange={(currency)=>{
+                              setAmount(amount)
+                              setFrom(currency)
+                            }}
+                            selectCurrency= {from}
+                            onAmountChange = {(amount)=> setAmount(amount)}
+
+                        />
+                    </div>
+                    <div className="relative w-full h-0.5">
+                        <button
+                            type="button"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                            onClick={swap}
+                        >
+                            swap
+                        </button>
+                    </div>
+                    <div className="w-full mt-1 mb-4">
+                        <InputBox
+                            label="To"
+                            amount={convertedAmount}
+                            currencyOptions={currencyOptions}
+                            onCurrencyChange={(currency)=>{
+                              setTo(currency)
+                            }}
+                            selectCurrency= {to}
+                          amountDisable
+                        />
+                    </div>
+                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                        Convert {from} to {to}
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+);
+}
+
+export default App
